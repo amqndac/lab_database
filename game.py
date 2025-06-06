078d043e816403bb8a74288b25f7fb702bde1995
amanda <0014051@student.isf.edu.hk>
2025-06-07 07:56:20 +0800

    have the basics of the game down, will then add difficulty levels

--- game.py
+++ game.py
@@ -1,10 +1,39 @@
 # this file should run a riddle guessing game
 
 from helpers import *
+import colorama
+from colorama import Fore, Back, Style 
+from colorama import init 
+init(autoreset=True)
 
 all_trivias = get_all_riddles()   
 
-for trivia in all_trivias:
-    print(f"--Trivia #{trivia['id']}")
+name = input("What is your name? \n")
+score = 0
+
+print("--------------------------------------------------\n" +
+f"Hello {name}, welcome to the Riddler! \n" +
+ "You will be asked a series of riddles.\n" +
+ "Try your best to answer correctly!\n" +
+ "--------------------------------------------------\n")
+
+riddles = get_random_riddles(5)
+
+for riddle in riddles: 
+   print(riddle['question'])
+   user_answer = input()
+   if user_answer == riddle['answer']:
+       print(Fore.GREEN + "Correct! ✅")
+       score += 1 
+   else:
+       print(Fore.RED + "Sorry, that is Incorrect. 😔")
+
+print("--------------------------------------------------\n" +
+f"Good job on finishing the game, {name}! \n" +
+f"Your score is: {score}/5 \n" +
+f"Try your best to answer correctly!\n"
+f"--------------------------------------------------\n")
+
+
 
 
--- helpers.py
+++ helpers.py
@@ -20,28 +20,52 @@ def get_all_riddles():
     return all_riddles
 
 def increment_row_value(column, id):
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+
+    # Only allow certain columns to prevent SQL injection
+    allowed_columns = ("total_guesses", "correct_guesses")
+    if column not in allowed_columns:
+        raise ValueError("Invalid column name")
+
+    conn.execute(
+        f"UPDATE riddles SET {column} = {column} + 1 WHERE id = ?",
+        (id,)
+    )
+    conn.commit()
+    conn.close()
+
+def get_random_riddles(num_riddles):
     conn = sqlite3.connect('database.db') # connect to database
     conn.row_factory = sqlite3.Row  #converts row to dictionary object
 
-    conn.execute(
-            f"""
-            UPDATE songs 
-            SET {column} = {column} + 1 
-            WHERE id = {id}"""
-        )
-    
-    conn.commit()
+    random_riddles = conn.execute(
+        """
+        SELECT * FROM riddles 
+        ORDER BY random()
+        LIMIT ?""",
+        (num_riddles,)
+    ).fetchall()
+
     conn.close()
+
+    return random_riddles
+
 
 
 if __name__=="__main__":
     # -- testing helper SQL functions
 
-    all_riddles = get_all_riddles()
-    for riddle in all_riddles:
+    # all_riddles = get_all_riddles()
+    # for riddle in all_riddles:
+    #     print(riddle['id'], riddle['question'])
+
+
+    increment_row_value('total_guesses', 1)
+
+    random_riddles = get_random_riddles(3)
+    for riddle in random_riddles: 
         print(riddle['id'], riddle['question'])
 
-
-    increment_row_value('total_guesses', id)
 
     
